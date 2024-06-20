cd35659ff2cb900ad136d5c48088b00c35049035
jisooooooooooo <dlawltn0708@naver.com>
2024-06-20 23:50:03 +0900

    Feat: 메인 페이지 일부 구현

---
+++ components/Header.js
@@ -0,0 +1,35 @@
+import { View, Text, StyleSheet, Image } from "react-native";
+
+const Header = () => {
+  return (
+    <View style={styles.header}>
+      <Image source={require("../assets/logo.png")} style={styles.logo} />
+      <Text style={styles.logotext}>CLONING</Text>
+    </View>
+  );
+};
+
+export default Header;
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: "#fff",
+    height: 44,
+  },
+  logo: {
+    width: 29,
+    height: 25,
+    marginRight: 4,
+  },
+  logotext: {
+    fontSize: 20,
+    fontFamily: "Luckiest Guy",
+    color: "#6269D4",
+    lineHeight: "19.36",
+  },
+});
