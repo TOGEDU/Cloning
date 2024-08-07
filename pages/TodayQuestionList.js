ff2f38178dcec8a4537160fe455205a6a06c4540
jisooooooooooo <dlawltn0708@naver.com>
2024-08-07 21:53:20 +0900

    merge

--- pages/TodayQuestionList.js
+++ pages/TodayQuestionList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Text,
   View,
@@ -7,70 +7,53 @@ import {
   TouchableOpacity,
   ScrollView,
 } from "react-native";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import todayQListImage from "../assets/todayQList.png";
 import QImage from "../assets/Qimage.png";
 import AImage from "../assets/Aimage.png";
 import downIcon from "../assets/chevron-down.png";
 import upIcon from "../assets/chevron-up.png";
 
-const questionData = [
-  {
-    id: 1,
-    date: "2024.06.20",
-    question: "과거 처음의 일기..데이터 어떻게 쌓이는지 보자고",
-    answer:
-      "오늘 학교에서 친구와 다퉜다고 힘들었겠구나. 너의 마음이 어려웠을 거라고 알아. 친구와 다투는 건 어렵고 슬픈 일이지만, 때로는 그런 상황도 있어. 그럴 때마다 당황스러워하고 속상해하는 건 당연한 거야.",
-  },
-  {
-    id: 2,
-    date: "2024.06.21",
-    question: "아이가 학교에서 친구와 다투고 온 날 해주고 싶은말은 무엇인가요?",
-    answer:
-      "오늘 학교에서 친구와 다퉜다고 힘들었겠구나. 너의 마음이 어려웠을 거라고 알아. 친구와 다투는 건 어렵고 슬픈 일이지만, 때로는 그런 상황도 있어. 그럴 때마다 당황스러워하고 속상해하는 건 당연한 거야.",
-  },
-  {
-    id: 3,
-    date: "2024.06.22",
-    question: "22일 질문이 들어갈 내용입니다.",
-    answer: "22일 답변이 들어갈 내용입니다.",
-  },
-  {
-    id: 4,
-    date: "2024.06.23",
-    question: "23일 질문이 들어갈 내용입니다.",
-    answer: "23일 답변이 들어갈 내용입니다.",
-  },
-  {
-    id: 5,
-    date: "2024.06.24",
-    question: "24일 질문이 들어갈 내용입니다.",
-    answer: "24일 답변이 들어갈 내용입니다.",
-  },
-  {
-    id: 6,
-    date: "2024.06.25",
-    question: "25일 질문이 들어갈 내용입니다.",
-    answer: "25일 답변이 들어갈 내용입니다.",
-  },
-  {
-    id: 7,
-    date: "2024.06.26",
-    question: "아이가 학교에서 친구와 다투고 온 날 해주고 싶은말은 무엇인가요?",
-    answer:
-      "오늘 학교에서 친구와 다퉜다고 힘들었겠구나. 너의 마음이 어려웠을 거라고 알아. 친구와 다투는 건 어렵고 슬픈 일이지만, 때로는 그런 상황도 있어. 그럴 때마다 당황스러워하고 속상해하는 건 당연한 거야.",
-  },
-  // 추가적인 질문 데이터는 필요에 따라 추가합니다.
-];
-
 const TodayQuestionList = () => {
   const [expandedId, setExpandedId] = useState(null);
+  const [questionData, setQuestionData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = await AsyncStorage.getItem("authToken");
+
+        if (!token) {
+          console.error("Token not available");
+          return;
+        }
+
+        const response = await axios.get(
+          "http://192.168.35.124:8080/api/dailyquestion",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        console.log("API response:", response.data);
+
+        if (response.data) {
+          setQuestionData(response.data);
+        } else {
+          console.error("Error during response:", response.data);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const toggleExpand = (id) => {
-    if (expandedId === id) {
-      setExpandedId(null);
-    } else {
-      setExpandedId(id);
-    }
+    setExpandedId((prevId) => (prevId === id ? null : id));
   };
 
   return (
@@ -83,35 +66,38 @@ const TodayQuestionList = () => {
           </Text>
           <Image source={todayQListImage} style={styles.image} />
         </View>
-        {questionData
-          .slice(0)
-          .reverse()
-          .map((item) => (
-            <TouchableOpacity
-              key={item.id}
-              onPress={() => toggleExpand(item.id)}
-            >
-              <View style={styles.questionContainer}>
-                <View style={styles.questionHeader}>
-                  <Text style={styles.questionDate}>{item.date}</Text>
-                </View>
-                <View style={styles.questionContent}>
-                  <Image source={QImage} style={styles.Qicon} />
-                  <Text style={styles.questionText}>{item.question}</Text>
-                  <Image
-                    source={expandedId === item.id ? upIcon : downIcon}
-                    style={styles.icon}
-                  />
-                </View>
-                {expandedId === item.id && (
-                  <View style={styles.answerContainer}>
-                    <Image source={AImage} style={styles.Aicon} />
-                    <Text style={styles.answerText}>{item.answer}</Text>
+        {questionData.length > 0 &&
+          questionData
+            .slice(0)
+            .reverse()
+            .map((item) => (
+              <TouchableOpacity
+                key={item.questionId}
+                onPress={() => toggleExpand(item.questionId)}
+              >
+                <View style={styles.questionContainer}>
+                  <View style={styles.questionHeader}>
+                    <Text style={styles.questionDate}>{item.date}</Text>
                   </View>
-                )}
-              </View>
-            </TouchableOpacity>
-          ))}
+                  <View style={styles.questionContent}>
+                    <Image source={QImage} style={styles.Qicon} />
+                    <Text style={styles.questionText}>{item.question}</Text>
+                    <Image
+                      source={
+                        expandedId === item.questionId ? upIcon : downIcon
+                      }
+                      style={styles.icon}
+                    />
+                  </View>
+                  {expandedId === item.questionId && (
+                    <View style={styles.answerContainer}>
+                      <Image source={AImage} style={styles.Aicon} />
+                      <Text style={styles.answerText}>{item.text}</Text>
+                    </View>
+                  )}
+                </View>
+              </TouchableOpacity>
+            ))}
       </View>
     </ScrollView>
   );
@@ -126,9 +112,6 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   container: {
-    //paddingTop: 27,
-    //backgroundColor: "#F7F8FF",
-    //flex: 1,
     marginBottom: 75,
   },
   titleContainer: {
@@ -162,12 +145,6 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     borderRadius: 20,
   },
-  questionHeader: {
-    //flexDirection: "row",
-    //justifyContent: "space-between",
-    //alignItems: "center",
-    //marginBottom: 8,
-  },
   questionDate: {
     fontFamily: "NotoSans",
     fontSize: 12,
@@ -177,14 +154,12 @@ const styles = StyleSheet.create({
   questionContent: {
     flexDirection: "row",
     alignItems: "center",
-    //marginBottom: 10,
   },
   icon: {
     width: 16,
     height: 9,
     marginRight: 10,
   },
-
   questionText: {
     fontFamily: "NotoSans",
     fontSize: 15,
@@ -203,5 +178,13 @@ const styles = StyleSheet.create({
     lineHeight: 20,
     margin: 10,
     marginRight: 40,
+  },
+  Qicon: {
+    width: 24,
+    height: 24,
+  },
+  Aicon: {
+    width: 24,
+    height: 24,
   },
 });
